784ba0f86fa578406058715ef478874d55cb2a5b
itsTadae <tadaemusic@gmail.com>
2019-11-25 16:38:15 -0500

    Updates

---
+++ precache-manifest.489acfe18f6fa8e149e22cbe4d49fed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06f6e4a6968e305e0fa47c08c31b735",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d11edeb195d4c007d0bf",
+    "url": "/monsters-rolodex/static/css/main.49f27104.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monsters-rolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "d11edeb195d4c007d0bf",
+    "url": "/monsters-rolodex/static/js/main.727aef17.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
